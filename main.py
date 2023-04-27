1859cc4e11e896e486a7d4cfe2ce507b9a3b0c81
reecepounder <53270052+reecepounder@users.noreply.github.com>
2023-04-26 23:19:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import dmv_check
-# made it a module for debug purposes. repl.it moment
-dmv_check.check(2)
+# Check for all 3 letter permutations and make a file with the results
+dmv_check.check()
 
+# ... or you can use the returnValues parameter to return the results as a list instead
+#availablePlates = dmv_check(3, returnValues=True)
+#print(availablePlates)
+
+# ... You can also toggle output to console (defaults to __debug__ unless overridden).
+#dmv_check(3, printDebugStatements=False)
